8eb9d7a907e2082bc8b07d4b506b27c470bb6929
D.Coloma <dnl.coloma@gmail.coma>
2018-05-29 15:29:06 +0200

    [FIX] Vulnerabilities from dependancies

--- db/migrate/20180202013024_create_students.rb
+++ db/migrate/20180202013024_create_students.rb
@@ -2,6 +2,7 @@ class CreateStudents < ActiveRecord::Migration[5.1]
   def change
     create_table :students do |t|
       t.string :name
+      t.integer :course_id
 
       t.timestamps
     end
--- db/migrate/20180202013038_create_courses.rb
+++ db/migrate/20180202013038_create_courses.rb
@@ -2,7 +2,7 @@ class CreateCourses < ActiveRecord::Migration[5.1]
   def change
     create_table :courses do |t|
       t.string :title
-
+      
       t.timestamps
     end
   end
